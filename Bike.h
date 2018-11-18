943ce686e4ad3176e3e26bf74ca50a971e13305e
Oleh Kuzyk <o.kuzyk.2001@gmail.com>
2018-11-18 10:28:09 +0200

    declaration of Bike class that contains 8 fields and 6 getters

---
+++ Bike.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Bike {
+private:
+	int numberOfSpeeds;
+	int mass;
+	int maxMassOfPassenger;
+	string manufacturer;
+protected:
+	int numberOfWheels;
+	int heightOfSeat;
+public:
+	double diameterOfWheel;
+	string owner;
+
+	Bike();
+	Bike(int numberOfSpeeds, 
+		int mass, 
+		int maxMassOfPassenger, 
+		string manufacturer, 
+		double diameterOfWheel, 
+		string owner, 
+		int numberOfWheels, 
+		int heightOfSeat);
+	int getNumberOfSpeeds();
+	int getMass();
+	int getMaxMassOfPassenger();
+	string getManufacturer();
+	int getNumberOfWheels();
+	int getHeightOfSeat();
+	~Bike();
+};
