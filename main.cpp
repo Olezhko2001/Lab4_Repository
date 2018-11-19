37c062aca8be9534d77526369d7f361d3bc4ffd8
Oleh Kuzyk <o.kuzyk.2001@gmail.com>
2018-11-19 21:27:36 +0200

    clean code

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 #include "Bike.h"
 using namespace std;
 
-void getAllInfoAboutBike(Bike bike);
+void printAllInfoAboutBikeIntoConsole(Bike bike);
 
 int main()
 {
 	Bike bike1(15, 15, 100, "Ukraine", 40, "Vasyl", 2, 75);
 	Bike bike2(20, 10, 100, "Yamaha", 30, "Oleh", 2, 75);
 	Bike bike3(15, 7, 90, "Ocean", 27, "Julia", 2, 50);
-	getAllInfoAboutBike(bike1);
-	getAllInfoAboutBike(bike2);
-	getAllInfoAboutBike(bike3);
+	printAllInfoAboutBikeIntoConsole(bike1);
+	printAllInfoAboutBikeIntoConsole(bike2);
+	printAllInfoAboutBikeIntoConsole(bike3);
 }
 
-void getAllInfoAboutBike(Bike bike)
+void printAllInfoAboutBikeIntoConsole(Bike bike)
 {
 	cout << "\nOwner: " << bike.owner;
 	cout << "\nNumber of speeds: " << bike.getNumberOfSpeeds();
